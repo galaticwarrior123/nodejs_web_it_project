197d423e285c2897bd6dca689c51b6afed589a95
galaticwarrior123 <trongnguyenphuc22082003@gmail.com>
2023-10-14 15:44:32 +0700

    update file

--- src/app/models/Tro.js
+++ src/app/models/Tro.js
@@ -8,7 +8,7 @@ const tro = new Schema({
     page: { type: String, maxLength: 255 },
     slug: { type: String, maxLength: 255 },
     // description: { type: String, maxLength: 600 },
-    // image: { type: String, maxLength: 255 },
+    image: { type: Array, maxLength: 255 ,default:[]},
     // createAt: { type: Date, default: Date.now },
 });
 
